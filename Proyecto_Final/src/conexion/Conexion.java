d841162b6f90a42ed8a7c3cb3eaaf5ccc19d2a53
Caren <141648871+carenMMarkevicz@users.noreply.github.com>
2023-10-02 12:41:51 -0300

    conexionMDB

---
+++ Proyecto_Final/src/conexion/Conexion.java
@@ -0,0 +1,46 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+
+public class Conexion {
+
+    private static String url="jdbc:mariadb://localhost/appuniversidadulp";
+    private static String usuario="root";
+    private static String password="";
+
+   
+    private static Conexion conexion=null;
+    
+     private Conexion() {
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            
+            
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "Clase Conexion: Error al cargar Driver");
+        }
+    }
+
+
+    
+    public static Connection getConexion() {
+        Connection con=null;
+      if(conexion == null){
+          
+           conexion= new Conexion();
+        }
+        try {
+            con = DriverManager.getConnection(url + "?useLegacyDatetimeCode=false&serverTimezone=UTC" + "&user=" + usuario + "&password=" + password);
+            //JOptionPane.showMessageDialog(null, "conectado");
+            
+        }catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error de conexion ");
+        }
+        
+        return con;
+    }
+    
+}
